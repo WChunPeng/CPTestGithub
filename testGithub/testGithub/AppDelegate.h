de9e4e7a22dcddc79282affadf0394e85ed05391
WChunPeng <583078764@qq.com>
2018-05-07 11:22:45 +0800

    Add files via upload
    git init

---
+++ testGithub/testGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGithub
+//
+//  Created by Dev on 2018/5/7.
+//  Copyright © 2018年 Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGithub/testGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGithub
+//
+//  Created by Dev on 2018/5/7.
+//  Copyright © 2018年 Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
